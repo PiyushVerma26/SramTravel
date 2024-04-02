d4ca91663019672bbf86fb9973b38fed983cdc28
piyushverma26 <piyushv528@gmail.com>
2024-04-02 16:42:33 +0530

    Add Cab Card Component

--- src/dev/devPages/CabSRPCard/index.jsx
+++ src/dev/devPages/CabSRPCard/index.jsx
@@ -1,15 +1,27 @@
 import React from 'react'
 import Cabcard from '../../../pages/CabSRP/components/Cabcard'
-
+import volk from '../../../assets/images/volk.avif';
 function CabSrpcard() {
   return (
-    <div className='flex flex-wrap'> 
-        <Cabcard />
-        <Cabcard />
-        <Cabcard />
-        <Cabcard />
-        <Cabcard />
-
+    <div className='flex flex-wrap p-4 gap-3 w-full'> 
+        <Cabcard  name='Volkswaegon Polo'   door='3-4'  classes='Economy'  user='4'  luggage='1'  cooling='AC'  type='Automatic'  location='Dwarka Delhi Sector-22'  img={volk}  dealCount='1'  basePrice='4000'  finalPrice='3000'  cancellationType='Free Cancellation'/>
+         
+        <Cabcard  name='Volkswaegon Polo'   door='3-4'  classes='Economy'  user='4'  luggage='1'  cooling='AC'  type='Automatic'  location='Dwarka Delhi Sector-22'  img={volk}  dealCount='1'  basePrice='4000'  finalPrice='3000'  cancellationType='Free Cancellation'/>
+         
+        <Cabcard  name='Volkswaegon Polo'   door='3-4'  classes='Economy'  user='4'  luggage='1'  cooling='AC'  type='Automatic'  location='Dwarka Delhi Sector-22'  img={volk}  dealCount='1'  basePrice='4000'  finalPrice='3000'  cancellationType='Free Cancellation'/>
+         
+        <Cabcard  name='Volkswaegon Polo'   door='3-4'  classes='Economy'  user='4'  luggage='1'  cooling='AC'  type='Automatic'  location='Dwarka Delhi Sector-22'  img={volk}  dealCount='1'  basePrice='4000'  finalPrice='3000'  cancellationType='Free Cancellation'/>
+         
+        <Cabcard  name='Volkswaegon Polo'   door='3-4'  classes='Economy'  user='4'  luggage='1'  cooling='AC'  type='Automatic'  location='Dwarka Delhi Sector-22'  img={volk}  dealCount='1'  basePrice='4000'  finalPrice='3000'  cancellationType='Free Cancellation'/>
+         
+        <Cabcard  name='Volkswaegon Polo'   door='3-4'  classes='Economy'  user='4'  luggage='1'  cooling='AC'  type='Automatic'  location='Dwarka Delhi Sector-22'  img={volk}  dealCount='1'  basePrice='4000'  finalPrice='3000'  cancellationType='Free Cancellation'/>
+         
+        <Cabcard  name='Volkswaegon Polo'   door='3-4'  classes='Economy'  user='4'  luggage='1'  cooling='AC'  type='Automatic'  location='Dwarka Delhi Sector-22'  img={volk}  dealCount='1'  basePrice='4000'  finalPrice='3000'  cancellationType='Free Cancellation'/>
+         
+        <Cabcard  name='Volkswaegon Polo'   door='3-4'  classes='Economy'  user='4'  luggage='1'  cooling='AC'  type='Automatic'  location='Dwarka Delhi Sector-22'  img={volk}  dealCount='1'  basePrice='4000'  finalPrice='3000'  cancellationType='Free Cancellation'/>
+         
+        <Cabcard  name='Volkswaegon Polo'   door='3-4'  classes='Economy'  user='4'  luggage='1'  cooling='AC'  type='Automatic'  location='Dwarka Delhi Sector-22'  img={volk}  dealCount='1'  basePrice='4000'  finalPrice='3000'  cancellationType='Free Cancellation'/>
+         
     </div>
   )
 }
