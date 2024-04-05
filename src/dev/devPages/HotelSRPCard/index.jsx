bfa46a76c1bfd72edb887cd24811f7d5843988f3
ashwin-codes <contact@ashwincodes.com>
2024-04-05 15:18:41 +0530

    feat(): add husky pre-commit hook for automatically prettify code

--- src/dev/devPages/HotelSRPCard/index.jsx
+++ src/dev/devPages/HotelSRPCard/index.jsx
@@ -3,17 +3,95 @@ import Hotelcard from '../../../pages/HotelSRP/components/HotelCard'
 
 function HotelSrpCard() {
   return (
-    <div className='flex flex-wrap'> 
- <Hotelcard hotelName={'Zone Connect Calangute'}  rating='3.9' star='4' location='Gurugram' ratingCount='50' totalPrice='5000' basePrice={'4500'} tax='₹ 1313' off='15'/>
- <Hotelcard hotelName={'Zone Connect Calangute'}  rating='3.9' star='4' location='Gurugram' ratingCount='50' totalPrice='5000' basePrice={'4500'} tax='₹ 1313' off='15'/>
- <Hotelcard hotelName={'Zone Connect Calangute'}  rating='3.9' star='4' location='Gurugram' ratingCount='50' totalPrice='5000' basePrice={'4500'} tax='₹ 1313' off='15'/>
- <Hotelcard hotelName={'Zone Connect Calangute'}  rating='3.9' star='4' location='Gurugram' ratingCount='50' totalPrice='5000' basePrice={'4500'} tax='₹ 1313' off='15'/>
- <Hotelcard hotelName={'Zone Connect Calangute'}  rating='3.9' star='4' location='Gurugram' ratingCount='50' totalPrice='5000' basePrice={'4500'} tax='₹ 1313' off='15'/>
- <Hotelcard hotelName={'Zone Connect Calangute'}  rating='3.9' star='4' location='Gurugram' ratingCount='50' totalPrice='5000' basePrice={'4500'} tax='₹ 1313' off='15'/>
- <Hotelcard hotelName={'Zone Connect Calangute'}  rating='3.9' star='4' location='Gurugram' ratingCount='50' totalPrice='5000' basePrice={'4500'} tax='₹ 1313' off='15'/>
- <Hotelcard hotelName={'Zone Connect Calangute'}  rating='3.9' star='4' location='Gurugram' ratingCount='50' totalPrice='5000' basePrice={'4500'} tax='₹ 1313' off='15'/>
-
-
+    <div className='flex flex-wrap'>
+      <Hotelcard
+        hotelName={'Zone Connect Calangute'}
+        rating='3.9'
+        star='4'
+        location='Gurugram'
+        ratingCount='50'
+        totalPrice='5000'
+        basePrice={'4500'}
+        tax='₹ 1313'
+        off='15'
+      />
+      <Hotelcard
+        hotelName={'Zone Connect Calangute'}
+        rating='3.9'
+        star='4'
+        location='Gurugram'
+        ratingCount='50'
+        totalPrice='5000'
+        basePrice={'4500'}
+        tax='₹ 1313'
+        off='15'
+      />
+      <Hotelcard
+        hotelName={'Zone Connect Calangute'}
+        rating='3.9'
+        star='4'
+        location='Gurugram'
+        ratingCount='50'
+        totalPrice='5000'
+        basePrice={'4500'}
+        tax='₹ 1313'
+        off='15'
+      />
+      <Hotelcard
+        hotelName={'Zone Connect Calangute'}
+        rating='3.9'
+        star='4'
+        location='Gurugram'
+        ratingCount='50'
+        totalPrice='5000'
+        basePrice={'4500'}
+        tax='₹ 1313'
+        off='15'
+      />
+      <Hotelcard
+        hotelName={'Zone Connect Calangute'}
+        rating='3.9'
+        star='4'
+        location='Gurugram'
+        ratingCount='50'
+        totalPrice='5000'
+        basePrice={'4500'}
+        tax='₹ 1313'
+        off='15'
+      />
+      <Hotelcard
+        hotelName={'Zone Connect Calangute'}
+        rating='3.9'
+        star='4'
+        location='Gurugram'
+        ratingCount='50'
+        totalPrice='5000'
+        basePrice={'4500'}
+        tax='₹ 1313'
+        off='15'
+      />
+      <Hotelcard
+        hotelName={'Zone Connect Calangute'}
+        rating='3.9'
+        star='4'
+        location='Gurugram'
+        ratingCount='50'
+        totalPrice='5000'
+        basePrice={'4500'}
+        tax='₹ 1313'
+        off='15'
+      />
+      <Hotelcard
+        hotelName={'Zone Connect Calangute'}
+        rating='3.9'
+        star='4'
+        location='Gurugram'
+        ratingCount='50'
+        totalPrice='5000'
+        basePrice={'4500'}
+        tax='₹ 1313'
+        off='15'
+      />
     </div>
   )
 }
